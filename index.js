2727acc065f8f3c22e74b929ef27d0cfb9880459
squintas <salvador.frazao.quintas@gmail.com>
2021-06-28 16:56:46 +0100

    Button to refresh page

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
@@ -19,11 +18,9 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
 }
